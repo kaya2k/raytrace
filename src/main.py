9c5243387849042afaf87c4f2fe8987c1860b011
KAYA LEE <kayalee2k@gmail.com>
2025-06-08 00:49:46 +0900

    move sphere

--- src/main.py
+++ src/main.py
@@ -60,8 +60,9 @@ def create_scene():
     # Spheres (forming a tetrahedron arrangement)
     # add_sphere(center=(0, -80.0, 20), radius=20, color=COLOR_SPHERE)
     # add_sphere(center=(27.18, -80.0, -9.34), radius=20, color=COLOR_SPHERE)
-    add_sphere(center=(-11.82, -80.0, -18.21), radius=20, color=COLOR_SPHERE)
+    # add_sphere(center=(-11.82, -80.0, -18.21), radius=20, color=COLOR_SPHERE)
     # add_sphere(center=(5.12, -47.34, -2.52), radius=20, color=COLOR_SPHERE)
+    add_sphere(center=(0, -80.0, -18.21), radius=20, color=COLOR_SPHERE)
     # Cylinder
     # add_cylinder(center=(82, -92.5, -12), height=15, radius=24, color=COLOR_CYLINDER)
     add_cylinder(center=(82, -92.5, -10), height=17, radius=24, color=COLOR_CYLINDER)
