4ecf636e8ba79d3499f7bc724d3ddbf768353954
KAYA LEE <kayalee2k@gmail.com>
2025-06-08 01:03:06 +0900

    remove celling for test

--- src/main.py
+++ src/main.py
@@ -53,18 +53,18 @@ def create_scene():
     add_cube(center=(152.5, 0, 0), size=(5, 200, 205), color=COLOR_RED)  # right wall
     add_cube(center=(0, -102.5, 0), size=(310, 5, 205), color=COLOR_WHITE)  # floor
     # Ceiling split into four panels, leaving a rectangular opening (the light)
-    add_cube(center=(0, 102.5, 62.25), size=(310, 5, 80.5), color=COLOR_WHITE)
-    add_cube(center=(0, 102.5, -62.25), size=(310, 5, 80.5), color=COLOR_WHITE)
-    add_cube(center=(92, 102.5, 0), size=(126, 5, 44), color=COLOR_WHITE)
-    add_cube(center=(-92, 102.5, 0), size=(126, 5, 44), color=COLOR_WHITE)
+    # add_cube(center=(0, 102.5, 62.25), size=(310, 5, 80.5), color=COLOR_WHITE)
+    # add_cube(center=(0, 102.5, -62.25), size=(310, 5, 80.5), color=COLOR_WHITE)
+    # add_cube(center=(92, 102.5, 0), size=(126, 5, 44), color=COLOR_WHITE)
+    # add_cube(center=(-92, 102.5, 0), size=(126, 5, 44), color=COLOR_WHITE)
     # Spheres (forming a tetrahedron arrangement)
     # add_sphere(center=(0, -80.0, 20), radius=20, color=COLOR_SPHERE)
     # add_sphere(center=(27.18, -80.0, -9.34), radius=20, color=COLOR_SPHERE)
     # add_sphere(center=(-11.82, -80.0, -18.21), radius=20, color=COLOR_SPHERE)
     # add_sphere(center=(5.12, -47.34, -2.52), radius=20, color=COLOR_SPHERE)
+    # TEST
     add_sphere(center=(-11.82, -80.0, -10.0), radius=20, color=COLOR_SPHERE)
     add_sphere(center=(-30, -80.0, -10.0), radius=20, color=COLOR_SPHERE)
-    add_cube(center=(-30, -80.0, -10.0), size=(20, 20, 20), color=COLOR_SPHERE)
     # Cylinder
     # add_cylinder(center=(82, -92.5, -12), height=15, radius=24, color=COLOR_CYLINDER)
     add_cylinder(center=(82, -92.5, -10), height=17, radius=24, color=COLOR_CYLINDER)
