3ff952e536912ac12628b68c5a47eea9e3bf60db
KAYA LEE <kayalee2k@gmail.com>
2025-06-08 16:12:47 +0900

    test

--- src/core/round_cube.py
+++ src/core/round_cube.py
@@ -166,13 +166,12 @@ def normal_round_cube_device(
     dx = abs(lx) - he
     dy = abs(ly) - he
     dz = abs(lz) - he
-    print(f"dx: {dx}, dy: {dy}, dz: {dz}")
 
     qx = dx if dx > 0.0 else 0.0
     qy = dy if dy > 0.0 else 0.0
     qz = dz if dz > 0.0 else 0.0
 
-    if qx > 0.0 or qy > 0.0 or qz > 0.0:
+    if True: # qx > 0.0 or qy > 0.0 or qz > 0.0:
         length = math.sqrt(qx * qx + qy * qy + qz * qz)
         inv = 1.0 / length
         nx_local = qx * inv * (1.0 if lx >= 0.0 else -1.0)
@@ -189,7 +188,6 @@ def normal_round_cube_device(
         else:
             nx_local, ny_local, nz_local = 0.0, 0.0, (1.0 if lz >= 0.0 else -1.0)
 
-    print(f"nx_local: {nx_local}, ny_local: {ny_local}, nz_local: {nz_local}")
     nx = rot_mat[0, 0] * nx_local + rot_mat[1, 0] * ny_local + rot_mat[2, 0] * nz_local
     ny = rot_mat[0, 1] * nx_local + rot_mat[1, 1] * ny_local + rot_mat[2, 1] * nz_local
     nz = rot_mat[0, 2] * nx_local + rot_mat[1, 2] * ny_local + rot_mat[2, 2] * nz_local
