d574915e92990ad42331756778a0840442b44bcc
KAYA LEE <kayalee2k@gmail.com>
2025-06-08 23:57:22 +0900

    test env 0.1

--- src/core/scene.py
+++ src/core/scene.py
@@ -470,12 +470,12 @@ def trace_ray_device(
 
         if min_shape == -1:
             if hits_light_plane(ox, oy, oz, dx, dy, dz):
-                light_power = 30.0
+                light_power = 0.0
                 color_r += throughput_r * light_power
                 color_g += throughput_g * light_power
                 color_b += throughput_b * light_power
             else:
-                env_light = 0.0
+                env_light = 0.1
                 color_r += throughput_r * env_light
                 color_g += throughput_g * env_light
                 color_b += throughput_b * env_light
