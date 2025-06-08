f5642d8718512d324766a624e6fa144eeec0f4af
KAYA LEE <kayalee2k@gmail.com>
2025-06-09 01:41:34 +0900

    move

--- src/main.py
+++ src/main.py
@@ -116,7 +116,7 @@ def create_scene():
         (-76.59, -58.67, -14.37),
         (-70.10, -80.84, -7.87),
     ]
-    diff = np.array((4.0, 0, 1.0))
+    diff = np.array((-4.0, 0, 4.0))
     centers = [np.array(c) + diff for c in centers]
     for i in range(8):
         add_round_cube(center=centers[i],  rotation=rotations[i // 4])
