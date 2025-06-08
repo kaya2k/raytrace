03ef59dc055e23ab2cefd49b3c01ae879d4611f9
KAYA LEE <kayalee2k@gmail.com>
2025-06-08 21:20:54 +0900

    more light power

--- src/core/scene.py
+++ src/core/scene.py
@@ -471,9 +471,10 @@ def trace_ray_device(
 
         if min_shape == -1:
             if dz < 0.0:  # light source
-                color_r += throughput_r * 1.0
-                color_g += throughput_g * 1.0
-                color_b += throughput_b * 1.0
+                light_power = 5.0
+                color_r += throughput_r * light_power
+                color_g += throughput_g * light_power
+                color_b += throughput_b * light_power
             else:
                 env_r, env_g, env_b = 0.2, 0.2, 0.2
                 color_r += throughput_r * env_r
