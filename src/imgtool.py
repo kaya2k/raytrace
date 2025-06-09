762d05e86663a50695eb31c5ac97791a48d7970c
KAYA LEE <kayalee2k@gmail.com>
2025-06-10 00:57:38 +0900

    update imgtool

--- src/imgtool.py
+++ src/imgtool.py
@@ -1,12 +1,23 @@
+import os
 import numpy as np
 from PIL import Image
 from matplotlib import pyplot as plt
 
+INPUT_DIR = "report/img"
+OUTPUT_DIR = "report/img_for_poster"
+
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+
+def crop_and_save(input_path, output_path):
+    img = np.array(Image.open(input_path))
+    cropped = img[16:-16]
+    plt.imsave(output_path, cropped)
+
 
 if __name__ == "__main__":
-    IMG_PATH = "img/MIRROR.png"
-    NEW_PATH = "img/NEW.png"
-    img = np.array(Image.open(IMG_PATH))
-    img[:16] = 0.3
-    img[-16:] = 0.3
-    plt.imsave(NEW_PATH, img)
+    for filename in os.listdir(INPUT_DIR):
+        if filename.lower().endswith(".png"):
+            input_path = os.path.join(INPUT_DIR, filename)
+            output_path = os.path.join(OUTPUT_DIR, filename)
+            crop_and_save(input_path, output_path)
