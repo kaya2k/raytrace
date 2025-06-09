0ac78c20ac4dd731ee9a8b0daa12561b7eb9fbf9
KAYA LEE <kayalee2k@gmail.com>
2025-06-09 15:36:59 +0900

    test 2

--- src/core/utils.py
+++ src/core/utils.py
@@ -66,8 +66,7 @@ def sample_reflection(
     reflect_z = 2 * dot_nl * norm_z + in_dir_z
     reflect_x, reflect_y, reflect_z = normalize3(reflect_x, reflect_y, reflect_z)
 
-    roughness = 0.8 if shape_type == SHAPE_EGG else 0.7
-    blend = roughness * roughness
+    blend = 0.8 if shape_type == SHAPE_EGG else 0.6
 
     out_x = reflect_x * (1 - blend) + diff_x * blend
     out_y = reflect_y * (1 - blend) + diff_y * blend
